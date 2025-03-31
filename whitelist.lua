72ad07e1a451d9e4c063b89e664d87f167272f29
definezzz <107827720+Definezzz@users.noreply.github.com>
2025-03-31 17:22:31 +0500

    Create whitelist.lua

---
+++ whitelist.lua
@@ -0,0 +1,47 @@
+local base64 = require("gamesense/base64")
+local http = require('gamesense/http')
+
+local whitelist = {"1742785323"}
+local ffi = require("ffi")
+
+local function getObfuscatedHWID()
+    local type2 = ffi.typeof("void***")
+    local interface = client.create_interface("filesystem_stdio.dll", "VBaseFileSystem011")
+    local system10 = ffi.cast(type2, interface)
+    local gethwid = ffi.cast("long(__thiscall*)(void*, const char*, const char*)", system10[0][13])
+    local filecheck = "C:\\Windows\\Setup\\State\\State.ini"  -- Change this as necessary
+    local hwid = gethwid(system10, filecheck, "olympia")
+    return tostring(hwid)
+end
+
+local hwid = getObfuscatedHWID()
+
+-- Check if HWID is whitelisted
+local isWhitelisted = false
+for _, id in ipairs(whitelist) do
+    if hwid == id then
+        isWhitelisted = true
+        break
+    end
+end
+
+if isWhitelisted then
+    local link = "https://raw.githubusercontent.com/Definezzz/RobloxPlayerBeta/refs/heads/main/erased.lua"
+    http.get(link, function(success, response)
+        if not success then
+            print('Failed to fetch source URL')
+            return
+        end
+    
+        local data = response.body
+        local chunk, err = load(data)
+    
+        if chunk then
+            chunk()
+        else
+            print('Failed to load chunk: ' .. err)
+        end
+    end)
+else
+    print("HWID isn't in our database.")
+end
